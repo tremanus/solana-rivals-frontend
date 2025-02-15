e918b57d4225a80a1ca0ce5051174b3f6e6ca8c5
tremanus <lbiyabrook@gmail.com>
2025-02-14 21:35:58 -0800

    bbbbboooom

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'treehacks.com',
+        pathname: '/favicon.ico',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
